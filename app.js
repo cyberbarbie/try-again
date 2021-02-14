3db245a0842920e46bb94c28075060d422841fb7
cyberbarbie <taeluralexis@gmail.com>
2021-02-14 06:33:04 +0000

    updated app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('');
+	res.send('This doesnt work anymore');
 });
 
-app.listen(port, () => console.log(`${port}!`));
+app.listen(port, () => console.log(`Port was supposed to run at ${port}!`));
