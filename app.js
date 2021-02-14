1167b8d06b88b82cc678b1419d0509b9f16adcb3
cyberbarbie <taeluralexis@gmail.com>
2021-02-14 06:44:46 +0000

    match up with server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('This doesnt work anymore');
+	res.send('bruh nvm it do work. Take notessss!');
 });
 
 app.listen(port, () => console.log(`Port was supposed to run at ${port}!`));
