e033d271a036faebcbe8e64fc7fdc634b35430ab
cyberbarbie <taeluralexis@gmail.com>
2021-02-14 05:59:55 +0000

    full commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World, this is my final fucking attempt');
+});
+
+app.listen(port, () => console.log(`This bitch is running on port ${port}!`));
